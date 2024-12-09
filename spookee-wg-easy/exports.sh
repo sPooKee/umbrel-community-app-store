a7fbdcb8f1ba6a19f67d79ba71a8ce188a58a946
Christian Proske <chris@pr0ske.de>
2024-12-09 16:54:25 +0100

    fix app_proxy

--- spookee-wg-easy/exports.sh
+++ spookee-wg-easy/exports.sh
@@ -1,5 +1,5 @@
 export APP_WGEASY_DATA_DIR="${EXPORTS_APP_DIR}/data/wireguard"
-export APP_WGEASY_UI_PORT="51821"
-export APP_WGEASY_SRV_PORT="51820"
+#export APP_WGEASY_UI_PORT="51821"
+#export APP_WGEASY_SRV_PORT="51820"
 
 export APP_PASSWORD_HASH="$(htpasswd -bnBC 12 "" TEST | tr -d ':\n')"
